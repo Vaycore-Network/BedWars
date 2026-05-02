a88832a964ee5ca4784c5302240c5598c2da2c1b
c4vxl <manage@c4vxl.de>
2026-05-02 17:28:00 +0200

    Created spawner holograms

--- src/main/kotlin/de/c4vxl/bedwars/data/Spawner.kt
+++ src/main/kotlin/de/c4vxl/bedwars/data/Spawner.kt
@@ -1,10 +1,17 @@
 package de.c4vxl.bedwars.data
 
 import de.c4vxl.bedwars.handler.ItemTranslationHandler.Companion.translatable
+import de.c4vxl.bedwars.utils.HologramUtils.createHologram
+import de.c4vxl.bedwars.utils.HologramUtils.setHologramItem
+import de.c4vxl.bedwars.utils.HologramUtils.setHologramText
+import de.c4vxl.gamemanager.gma.game.Game
+import de.c4vxl.gamemanager.language.Language.Companion.language
 import de.c4vxl.gamemanager.utils.ItemBuilder
+import net.minecraft.world.entity.decoration.ArmorStand
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.World
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.util.Vector
 
@@ -16,15 +23,15 @@ data class Spawner(
     val material: Material,
     val interval: Int,
     val displayDelay: Boolean,
-    val name: String?,
-    var timeWaited: Int = 0
+    val displayName: String?,
+    val displayItem: Material?
 ) {
     /**
      * Builds the item
      */
     val item: ItemStack = ItemBuilder(material)
             // Add translation key
-            .apply { this@Spawner.name?.let { translatable(it) } }
+            .apply { translatable("item.material.${material.name.lowercase()}.name") }
             .build()
 
     /**
@@ -38,4 +45,42 @@ data class Spawner(
             // Remove random velocity from item
             .velocity = Vector(0, 0, 0)
     }
+
+    /**
+     * Sends the spawner hologram to a player
+     * @param player The player to send the hologram to
+     */
+    fun sendHologram(player: Player): ArmorStand? {
+        if (!displayDelay)
+            return null
+
+        val loc = location.clone().add(0.0, 1.5, 0.0)
+
+        // Send spawner display item
+        displayItem?.let {
+            player.setHologramItem(
+                player.createHologram(loc.clone().subtract(0.0, 0.25, 0.0)) ?: return@let,
+                it
+            )
+        }
+
+        // Send spawner name
+        displayName?.let {
+            player.setHologramText(
+                player.createHologram(loc.clone().add(0.0, 0.3, 0.0)) ?: return@let,
+                player.language.child("bedwars").getCmp(it)
+            )
+        }
+
+        return player.createHologram(loc)
+    }
+
+    /**
+     * Renders the spawner to all players of a game
+     * @param game The game to render the spawner to
+     */
+    fun render(game: Game) =
+        game.players.mapNotNull { player ->
+            player.bukkitPlayer to (sendHologram(player.bukkitPlayer) ?: return@mapNotNull null)
+        }
 }
--- src/main/kotlin/de/c4vxl/bedwars/handler/SpawnerHandler.kt
+++ src/main/kotlin/de/c4vxl/bedwars/handler/SpawnerHandler.kt
@@ -2,8 +2,11 @@ package de.c4vxl.bedwars.handler
 
 import de.c4vxl.bedwars.Main
 import de.c4vxl.bedwars.data.Spawner
+import de.c4vxl.bedwars.utils.HologramUtils.setHologramText
+import de.c4vxl.gamemanager.gma.event.game.GameStartedEvent
 import de.c4vxl.gamemanager.gma.event.game.GameWorldLoadedEvent
 import de.c4vxl.gamemanager.gma.event.game.GameWorldUnloadEvent
+import de.c4vxl.gamemanager.language.Language.Companion.language
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
@@ -21,21 +24,17 @@ class SpawnerHandler : Listener {
     }
 
     @EventHandler
-    fun onWorldLoaded(event: GameWorldLoadedEvent) {
-        val world = event.map.world ?: return
+    fun onStarted(event: GameStartedEvent) {
+        val world = event.game.worldManager.map?.world ?: return
 
-        // Read spawner data
-        val config = event.map.getMetadata("bedwars.spawners") ?: return
-        val spawners = config.getKeys(false).mapNotNull { key ->
-            val (x, y, z) = config.getIntegerList("$key.location")
-            Spawner(
-                Location(world, x + 0.5, y + 0.5, z + 0.5),
-                config.getString("$key.material")?.let { Material.getMaterial(it) } ?: return@mapNotNull null,
-                config.getInt("$key.interval"),
-                config.getBoolean("$key.display"),
-                config.getString("$key.name"),
-            )
+        // Get spawners
+        val spawners: List<Spawner> = event.game.gameData["spawners"] ?: run {
+            Main.logger.warning("Failed to access spawners of game ${event.game.id}")
+            return
         }
+
+        // Create holograms
+        val holograms = spawners.associateWith { it.render(event.game) }
 
         // Start spawners
         val timeWaited: MutableMap<Spawner, Int> = mutableMapOf()
@@ -47,13 +46,39 @@ class SpawnerHandler : Listener {
                 val waited = timeWaited[spawner] ?: 0
                 timeWaited[spawner] = waited + 1
 
+                // Update holograms
+                val dwait = spawner.interval - waited
+                holograms[spawner]?.forEach { (player, entity) ->
+                    player.setHologramText(entity, player.language.child("bedwars").getCmp("game.spawner.display.name",
+                        "${dwait.takeIf { it != 0 } ?: spawner.interval}"))
+                }
+
                 // Spawn item
                 if (waited >= spawner.interval) {
-                    timeWaited[spawner] = 0
+                    timeWaited[spawner] = 1
                     spawner.drop(world)
                 }
             }
         }, 20, 20)
+    }
+
+    @EventHandler
+    fun onWorldLoaded(event: GameWorldLoadedEvent) {
+        val world = event.map.world ?: return
+
+        // Read spawner data
+        val config = event.map.getMetadata("bedwars.spawners") ?: return
+        event.game.gameData["spawners"] = config.getKeys(false).mapNotNull { key ->
+            val (x, y, z) = config.getIntegerList("$key.location")
+            Spawner(
+                Location(world, x + 0.5, y + 0.5, z + 0.5),
+                config.getString("$key.material")?.let { Material.getMaterial(it) } ?: return@mapNotNull null,
+                config.getInt("$key.interval"),
+                config.getBoolean("$key.display.enabled"),
+                config.getString("$key.display.name"),
+                config.getString("$key.display.material")?.let { Material.getMaterial(it) }
+            )
+        }
     }
 
     @EventHandler
