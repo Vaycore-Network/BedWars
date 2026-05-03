c4469187f0ee639847f601a71e3968e70cfc313c
c4vxl <manage@c4vxl.de>
2026-05-03 16:10:02 +0200

    Added armor rules and modified game rules on world load

--- src/main/kotlin/de/c4vxl/bedwars/handler/GameHandler.kt
+++ src/main/kotlin/de/c4vxl/bedwars/handler/GameHandler.kt
@@ -2,9 +2,16 @@ package de.c4vxl.bedwars.handler
 
 import de.c4vxl.bedwars.Main
 import de.c4vxl.gamemanager.gma.event.game.GameStartedEvent
+import de.c4vxl.gamemanager.gma.event.game.GameWorldLoadedEvent
+import de.c4vxl.gamemanager.gma.player.GMAPlayer.Companion.gma
 import org.bukkit.Bukkit
+import org.bukkit.GameRules
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.player.PlayerSwapHandItemsEvent
+import kotlin.time.measureTime
 
 /**
  * Handles general game events
@@ -21,5 +28,25 @@ class GameHandler : Listener {
             event.game.worldManager.map?.name ?: "",
             child = "bedwars"
         )
+    }
+
+    @EventHandler
+    fun onWorldLoaded(event: GameWorldLoadedEvent) {
+        val world = event.map.world ?: return
+
+        world.setGameRule(GameRules.KEEP_INVENTORY, true) // Item dropping will be handled separately
+        world.setGameRule(GameRules.RANDOM_TICK_SPEED, 0)
+        world.time = 6000
+    }
+
+    @EventHandler
+    fun onArmor(event: InventoryClickEvent) {
+        val player = event.whoClicked as? Player ?: return
+        val game = player.gma.game ?: return
+
+        if (event.slot !in 36..39)
+            return
+
+        event.isCancelled = true
     }
 }
